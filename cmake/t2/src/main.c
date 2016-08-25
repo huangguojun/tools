16d130483625e0f7764eda232103bd5c7e2a925e
goujun.huang <goujun.huang@maiago.com>
2016-08-25 15:56:18 +0800

    add cmake

---
+++ cmake/t2/src/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  hello<CURSOR> 
+ *
+ *        Version:  1.0
+ *        Created:  06/23/2015 03:14:37 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  guojun.huang (hgj), guojun0921@gmail.com
+ *        Company:  PengBang,Shenzhen
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main()
+{
+	printf("Hello World from t1 Main!\n");
+	return 0;
+}
+
+
