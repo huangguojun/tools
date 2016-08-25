f45e6f64d7da22124ec0e6f12d4f8e855321ceb6
goujun.huang <goujun.huang@maiago.com>
2016-08-25 16:06:30 +0800

    add install_env.sh

---
+++ install_env.sh
@@ -0,0 +1,9 @@
+sudo apt-get install make gcc g++ 
+sudo apt-get install cmake 
+sudo apt-get install vim 
+sudo apt-get install tmux 
+sudo apt-get install exuberant-ctags
+sudo apt-get install scdv
+sudo apt-get install ckermit 
+
+
