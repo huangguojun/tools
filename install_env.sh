5001693d1d187ca08b3da202733ff384d918c661
guojun.huang <guojun.huang@qq.com>
2019-01-12 13:14:57 +0800

    update

--- install_env.sh
+++ install_env.sh
@@ -4,10 +4,12 @@ sudo apt-get install vim
 sudo apt-get install tmux 
 sudo apt-get install exuberant-ctags
 sudo apt-get install scdv
-sudo ln -s /usr/bin/sdcv /usr/bin/dict
+sudo ln -s /usr/bin/dict /usr/bin/sdcv
 sudo apt-get install ckermit 
 sudo apt-get install zsh curl
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+git config --global user.email "guojun.huang@qq.com"     
+git config --global user.name  "guojun.huang" 
 git config --global core.editor vim
