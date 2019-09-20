8b56229dd1121f24ce7d63cc804a99b79af22148
guojun.huang <guojun.huang@qq.com>
2019-09-20 19:59:48 +0800

    update

--- install_env.sh
+++ install_env.sh
@@ -8,7 +8,9 @@ sudo ln -s /usr/bin/dict /usr/bin/sdcv
 sudo apt-get install ckermit 
 sudo apt-get install zsh curl
 
+sudo rm ~/.oh-my-zsh -r
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
 
 git config --global user.email "guojun.huang@qq.com"     
 git config --global user.name  "guojun.huang" 
