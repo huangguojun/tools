37b2e947a93de95e3000ed6584ff9882c01b4848
guojun.huang <guojun.huang@qq.com>
2022-05-22 09:58:56 +0800

    update insatll_env.sh

--- install_env.sh
+++ install_env.sh
@@ -1,24 +1,20 @@
 sudo apt-get install make gcc g++ 
-sudo apt-get install qt5-default qttools5-dev 
+
+sudo apt-get install qttools5-dev qtbase5-dev qtdeclarative5-dev
 sudo apt-get install qtmultimedia5-dev qtscript5-dev libqt5xmlpatterns5-dev
+
 sudo apt-get install cmake cmake-curses-gui extra-cmake-modules
 sudo apt-get install vim git subversion
 sudo apt-get install tmux  tmuxinator 
 sudo apt-get install exuberant-ctags
 sudo apt-get install clang-format
+
 sudo apt-get install nvidia-cuda-dev nvidia-cuda-toolkit
 sudo apt-get install libboost-all-dev
 
-#sudo apt-get install scdv
-#sudo ln -s /usr/bin/dict /usr/bin/sdcv
+sudo apt-get install sdcv
 #sudo apt-get install ckermit 
 sudo apt-get install zsh curl
 
 chsh -s /bin/zsh
-
-git config --global user.email "guojun.huang@qq.com"     
-git config --global user.name  "guojun.huang" 
-git config --global core.editor vim
-git config --global diff.tool vimdiff
-git config --global difftool.prompt No
 
