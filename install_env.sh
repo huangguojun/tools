a1c665f6893fbb54dca1979e0479dc11bd850895
guojun <guojun.huang@qq.com>
2022-06-09 15:11:22 +0800

    Update install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -37,3 +37,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cp vim/.vim/autoload/plug.vim ~/.local/share/nvim/site/autoload/
