2f3c80fcc437dd62162b4e085918b2b96ad4bf2a
guojun.huang <guojun.huang@united-imaging.com>
2022-06-17 10:33:18 +0800

    update

--- install_env.sh
+++ install_env.sh
@@ -1,21 +1,21 @@
 # develop
 sudo apt-get install make gcc g++ 
+sudo apt-get install clang clang-tidy clang-tools clang-format cmake cppcheck doxygen graphviz
+sudo apt-get install cmake cmake-curses-gui extra-cmake-modules
 
+# qt
 sudo apt-get install qttools5-dev qtbase5-dev qtdeclarative5-dev
 sudo apt-get install qtmultimedia5-dev qtscript5-dev libqt5xmlpatterns5-dev
 sudo apt-get install qml-module-qtquick-controls2
 
-sudo apt-get install cmake cmake-curses-gui extra-cmake-modules
 sudo apt-get install vim git subversion git-extras
 sudo apt-get install tmux  tmuxinator 
 sudo apt-get install exuberant-ctags
-sudo apt-get install clang-format
 
 sudo apt-get install nvidia-cuda-dev nvidia-cuda-toolkit
 sudo apt-get install libboost-all-dev
 
 sudo apt-get install sdcv
-#sudo apt-get install ckermit 
 sudo apt-get install curl
 sudo apt-get install fzf
 sudo apt-get install ranger
